8c52e0798bcdfc0a602350b1c67a3c815e07da3c
schweinsbergs <schweinsbergs@csc4252021.ssdt.berea.edu>
2021-08-31 19:02:50 -0400

    just figured out how to print and add a file.

---
+++ coding.3/quest1.c
@@ -0,0 +1,16 @@
+/*** help from: https://computer.howstuffworks.com/c6.htm to figure out how to print... :| 
+***/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()  {
+
+int counter = 100; 
+
+printf("%d",counter);
+
+return 0;
+
+}
+
