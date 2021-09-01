67ffa8b50621eb6ef07e405507dda45fc45b4de9
schweinsbergs <schweinsbergs@csc4252021.ssdt.berea.edu>
2021-08-31 20:50:36 -0400

    Added 2 more blank files for 4 and 5.

---
+++ coding.3/quest5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+/***
+
+ Now write a program that uses wait() to wait for the child process
+to finish in the parent. What does wait() return? What happens if
+you use wait() in the child?
+
+***/ 
