1768ef9e1bfc2a723cbd23bacc8c1af87a35af1f
schweinsbergs <schweinsbergs@csc4252021.ssdt.berea.edu>
2021-08-31 21:04:02 -0400

    Added an answer for quest3

--- coding.3/quest3.c
+++ coding.3/quest3.c
@@ -2,14 +2,38 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int main()  {
+
+
+int rc = fork();
+
+if (rc == 0) {
+
+printf("Hello! I'm the child!"); 
+
+} else {
+printf("Goodbye. I'm the parent.");
+
+}
+
+return 0;
+
+}
 
 
 
 /***
 
+Sam Schweinsberg
+
 Write another program using fork(). The child process should
 print “hello”; the parent process should print “goodbye”. You should
 try to ensure that the child process always prints first; can you do
 this without calling wait() in the parent?
+
+I'm not quite sure that I can do this without using wait-- 
+The thing I tried was having the parent say goodbye last, but it doesn't
+seem like that wants to work...  To my understanding the parent just 
+skips over the child's code and executes first? 
 
 ***/
