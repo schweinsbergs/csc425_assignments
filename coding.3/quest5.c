967c4b167dadc7cd3f607768ada4f8f2d38681eb
schweinsbergs <schweinsbergs@csc4252021.ssdt.berea.edu>
2021-09-01 20:49:58 -0400

    Added answer for question 5.

--- coding.3/quest5.c
+++ coding.3/quest5.c
@@ -1,6 +1,23 @@
+/** We spent like 2 class periods walking through parts of this code to understand it, it's from the 5th chapter of the book. **/ 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+int main(int argc, char *argv[]) {
+printf("hello world (pid:%d)\n", (int) getpid());
+int rc = fork();
+if (rc < 0) { // fork failed; exit
+fprintf(stderr, "fork failed\n");
+exit(1);
+} else if (rc == 0) { // child (new process)
+printf("hello, I am child (pid:%d)\n", (int) getpid());
+} else { // parent goes down this path (main)
+int rc_wait = wait(NULL);
+printf("hello, I am parent of %d (rc_wait:%d) (pid:%d)\n",
+rc, rc_wait, (int) getpid());
+}
+ return 0;
 
 
 /***
@@ -8,5 +25,9 @@
  Now write a program that uses wait() to wait for the child process
 to finish in the parent. What does wait() return? What happens if
 you use wait() in the child?
+
+Wait returns whatever the child was working on so the parent can continue. You can't really use wait for a child, it's 
+bad practice. The parent and child would be able to wait for each other forever, so you might need to reboot. 
+
 
 ***/ 
