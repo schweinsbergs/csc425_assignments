bc648e1f4370375965990cb9d2e62d1ee359e5c5
schweinsbergs <schweinsbergs@csc4252021.ssdt.berea.edu>
2021-09-01 20:09:21 -0400

    Added question 2 answer

--- coding.3/quest2.c
+++ coding.3/quest2.c
@@ -1,4 +1,6 @@
-/*** help from: https://www.cs.utah.edu/~germain/PPS/Topics/C_Language/file_IO.html to read files ***/ 
+/*** help from: https://www.cs.utah.edu/~germain/PPS/Topics/C_Language/file_IO.html to read files 
+https://scanftree.com/programs/c/to-read-a-text-file-print-it-on-the-screen-in-c/
+***/ 
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,23 +8,25 @@
 
 int main()  {
 
-FILE* a = fopen("testfile.txt", "r");
-
+FILE* a;
+a  = fopen("./testfile.txt", "r");
+char b;
+b = getc(a);
 
 int rc = fork();
 if(rc < 0) {
-fprintf(stderr,"fork failed\n");
+printf("fork failed\n");
 exit(1); 
 
 } else if (rc == 0) {
 
-print(a);
-print("This is child reading!");
+printf("%c",b);
+printf("This is child reading!");
 
 } else {
 
-printf(a);
-print("This is parent reading!"); 
+printf("%c",b);
+printf("This is parent reading!"); 
 }
 
 return 0;
@@ -41,6 +45,9 @@ and parent access the file descriptor returned by open()? What
 happens when they are writing to the file concurrently, i.e., at the
 same time?
 
+Both the child and the parent are able to access that file, which makes sense, because the child is a copy of the parent. I'm forking after 
+the file read is called. If they're writing to the file at the same time, and they're not taking turns or playing nice, they might 
+write over each other. 
 
 ***/ 
 
